e806867fe4bda425933c7ccb4e8f3ab90a4c55c2
Srujan Pulli <spulli@spwestpros.com>
2017-10-09 19:58:16 -0700

    updated script to hide Quick Launch

--- hideQuickLaunch/src/webparts/hideQuickLaunch/App.js
+++ hideQuickLaunch/src/webparts/hideQuickLaunch/App.js
@@ -1,17 +1,7 @@
 $.fn.hideQuickLaunch = function ()
-{
-    // nav.ms-Nav.root_fed0c9a7
-
-    // document.write("<script type=\"text/javascript\" src=\"https://code.jquery.com/jquery-3.2.1.min.js\"><\/script>");
-    
+{    
+    // css updates to hide quicklaunch.
     $( "nav[role='navigation']" ).hide();
-    $("div[class^='belowSearchBox_']").hide()
-    $("div[class^='pageContainer_']").css( "left", "0px" );    
-    
-    // nav_97226a7d spNav_6602f88f belowSearchBox_6602f88f
-    // pageContainer_97226a7d container_97226a7d
-    // $("div[class^='apple-'],div[class*=' apple-']")
-    
-    
-
+    $("div[class^='searchBox_']").hide();
+    $("div[class^='pageContainer_']").css( "left", "0px" );   
 }
