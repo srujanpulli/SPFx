7f2fad8a6e46564ab8d5cfcb6808726dc24ec126
Srujan Pulli <spulli@spwestpros.com>
2017-10-16 18:13:42 -0700

    add a new solution to hide multiple elements e.g. quicklaunch or title or top nav

---
+++ hideAnElement/src/webparts/hideAnElement/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHideAnElementWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HideAnElementWebPartStrings' {
+  const strings: IHideAnElementWebPartStrings;
+  export = strings;
+}
