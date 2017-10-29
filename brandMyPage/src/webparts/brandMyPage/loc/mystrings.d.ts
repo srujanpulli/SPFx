f732d76847885e4df78332ca179142141d3a123c
Srujan Pulli <spulli@spwestpros.com>
2017-10-29 07:17:53 -0700

    added a new solution - Band My Page

---
+++ brandMyPage/src/webparts/brandMyPage/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBrandMyPageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BrandMyPageWebPartStrings' {
+  const strings: IBrandMyPageWebPartStrings;
+  export = strings;
+}
